992bd92ebfbbdc6da73c2c3515959ce6a7f000fe
hexstr <arraytoobig@gmail.com>
2022-12-14 17:16:49 +0800

    Add .clang-format; use clangd instead of ms intellisense; update CMakeLists.txt.

---
+++ HelloCMake/hello_cmake.hh
@@ -0,0 +1,13 @@
+﻿// hello_cmake.hh: 标准系统包含文件的包含文件
+// 或项目特定的包含文件。
+
+#ifndef HELLO_CMAKE_HEADER
+#define HELLO_CMAKE_HEADER
+
+#include <android/log.h>
+#define LOG_TAG "hexstr"
+#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
+
+#endif // hello_cmake.hh
+
+// TODO: 在此处引用程序需要的其他标头。
